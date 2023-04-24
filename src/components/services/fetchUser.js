02afbe7f594d7a1c39d2ad08a47cfce633206f22
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-24 22:30:51 +0300

    fetch user edit for pagination

--- src/components/services/fetchUser.js
+++ src/components/services/fetchUser.js
@@ -3,7 +3,9 @@ import axios from "axios";
 axios.defaults.baseURL = "https://64455cc4914c816083cd1353.mockapi.io/tweets";
 
 export const getUser = async () => {
+  const pagination = "limit=3&page=1";
+
   try {
-    return await axios.get("/users");
+    return await axios.get(`/users?${pagination}`);
   } catch (error) {}
 };
