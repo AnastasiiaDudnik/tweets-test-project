9236dea3e06b8b7c0c6fef08a46009860e8ba5f7
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-24 12:18:08 +0300

    on card  btn click

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,26 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 export const Card = ({ user: { name, tweets, avatar, followers } }) => {
+  const [follow, setFollow] = useState(false);
+
+  const toggleFollow = () => {
+    setFollow(!follow);
+  };
+
   return (
     <li>
       <img src={avatar} alt={name} />
       <p>{tweets} tweets</p>
       <p>{followers} followers</p>
-      <button type="button">Follow</button>
+
+      {follow ? (
+        <button type="button" onClick={toggleFollow}>
+          Following
+        </button>
+      ) : (
+        <button onClick={toggleFollow}>Follow</button>
+      )}
     </li>
   );
 };
