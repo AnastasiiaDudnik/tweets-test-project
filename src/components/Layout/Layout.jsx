65003cd11de056eedeed15d519cfa2f49e64a579
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-24 21:16:28 +0300

    add navigation component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
-import { NavLink } from "react-router-dom";
 import PropagateLoader from "react-spinners/PropagateLoader";
+import { Navigation } from "../Navigation/Navigation";
 
 export const Layout = () => {
   return (
     <div>
-      <header>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/tweets">Tweets</NavLink>
-        </nav>
-      </header>
-      <Suspense fallback={<PropagateLoader aria-label="Loading Spinner" />}>
-        <Outlet />
-      </Suspense>
+      <Navigation />
+      <main>
+        <Suspense fallback={<PropagateLoader aria-label="Loading Spinner" />}>
+          <Outlet />
+        </Suspense>
+      </main>
     </div>
   );
 };
