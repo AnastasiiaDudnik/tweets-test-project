5c63ec77d1b8852c2dfc88c2a85c43693880c7d0
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-25 20:31:36 +0300

    card list style update

--- src/components/CardList/CardList.styled.jsx
+++ src/components/CardList/CardList.styled.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const List = styled.ul`
   list-style: none;
   display: flex;
+  justify-content: center;
   gap: 48px;
   flex-basis: cacl((100% - 48px * 2) / 3);
 `;
