ef46055c745710089e56b884596fdb878d81ed81
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-26 20:07:41 +0300

    add styled to notFound component

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,21 @@
 import { GoBack } from "../GoBack/GoBack";
+import { Cat } from "./NotFound.styled";
 
 export const NotFound = () => {
   return (
     <div>
       <p>404</p>
       <p>Oops, seems like there's nothing here...</p>
+      <div>
+        <Cat
+          title="crying cat"
+          src="https://giphy.com/embed/3o7qEciAHeStgj1idG"
+          width="100%"
+          height="100%"
+          allowFullScreen
+        ></Cat>
+      </div>
+
       <GoBack />
     </div>
   );
