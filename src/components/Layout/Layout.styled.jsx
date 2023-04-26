5e93b8792d575b94b00d782073b984b1c0f203d6
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-26 19:12:29 +0300

    layout padding correction

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   max-width: 1200px;
   margin: 0 auto;
-  padding: 0 16px;
+  padding: 16px;
 `;
 
 export const MainContent = styled.main`
