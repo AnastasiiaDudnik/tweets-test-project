2575581014b86d8237611b9885eb404760b32e24
AnastasiiaDudnik <dudniknastya2000@gmail.com>
2023-04-26 19:48:01 +0300

    add NotFound component

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import { GoBack } from "../GoBack/GoBack";
+
+export const NotFound = () => {
+  return (
+    <div>
+      <p>404</p>
+      <p>Oops, seems like there's nothing here...</p>
+      <GoBack />
+    </div>
+  );
+};
